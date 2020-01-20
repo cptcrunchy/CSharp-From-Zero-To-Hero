cbc60a9614f7143f2b122569645edb8c212c569c
Jason Gutierrez <jasong84@gmail.com>
2020-01-20 16:16:04 -0600

    Extract bmi calculation functionality into Lesson3 class
    # |<----  Using a Maximum Of 50 Characters  ---->|
    
    
    # Explain why this change is being made
    # |<----   Try To Limit Each Line to a Maximum Of 72 Characters   ---->|
    
    # Provide links or keys to any relevant tickets, articles or other resources
    # Example: Github issue #23
    
    # --- COMMIT END ---
    # Type can be 
    #    feat     (new feature)
    #    fix      (bug fix)
    #    refactor (refactoring production code)
    #    style    (formatting, missing semi colons, etc; no code change)
    #    docs     (changes to documentation)
    #    test     (adding or refactoring tests; no production code change)
    #    chore    (updating grunt tasks etc; no production code change)
    # --------------------
    # Remember to
    #    Capitalize the subject line
    #    Use the imperative mood in the subject line
    #    Do not end the subject line with a period
    #    Separate subject from body with a blank line
    #    Use the body to explain what and why vs. how
    #    Can use multiple lines with "-" for bullet points in body
    # --------------------

---
+++ Src/BootCamp.Chapter/Lesson3.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace BootCamp.Chapter
+{
+	public static class Lesson3
+	{
+        public static void Demo()
+        {
+            WelcomePrompt();
+            do
+            {
+                PrompUserForStats();
+                
+            } while (PromptCalculateNewBMI());
+        }
+
+        public static bool PromptCalculateNewBMI()
+        {
+            Console.WriteLine("Do you wish to calculate another person? Y/N ");
+            string response = Console.ReadLine();
+            return response.ToLower().StartsWith("y");
+        }
+
+        public static void PrompUserForStats()
+        {
+
+            string name = PromptString("What is your name?");
+            int age = PromptInteger("How old are you? If I may ask");
+            float weight = PromptFloat("What is your weight? In Kilograms please.");
+            float height = PromptFloat("How tall are you? In Meters please.");
+            float calculatedBMI = CalculateBmi(weight, height);
+
+            Console.WriteLine($"{name} is {age} years old, weight is {weight} kg with a height of {height} cm.");
+
+            Console.WriteLine($"The calculated BMI  is: {calculatedBMI:F}");
+           
+        }
+
+        private static void WelcomePrompt()
+        {
+            Console.WriteLine("+------------------------------+");
+            Console.WriteLine("|                              |");
+            Console.WriteLine("|    Welcome to C# Bootcamp    |");
+            Console.WriteLine("|        BMI Calculator        |");
+            Console.WriteLine("|        Version 0.0.1         |");
+            Console.WriteLine("|                              |");
+            Console.WriteLine("+------------------------------+");
+        }
+
+        public static int PromptInteger(string message)
+        {
+            Console.WriteLine(message);
+            return int.Parse(Console.ReadLine());
+        }
+
+        public static float PromptFloat(string message)
+        {
+            Console.WriteLine(message);
+            return float.Parse(Console.ReadLine());
+        }
+       
+        public static string PromptString(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+
+        public static float CalculateBmi(float weight, float height)
+        {
+            return weight / MathF.Pow(height, 2);
+        }
+    }
+}
