f8ba2912f2cc2a04cddb13fd16f9329d19713bbc
Jason Gutierrez <jgutierrez@americanpayroll.org>
2020-02-03 09:15:04 -0600

    Implement Guard Clause on hw5 functions

--- Src/BootCamp.Chapter1/Lesson5.cs
+++ Src/BootCamp.Chapter1/Lesson5.cs
@@ -15,12 +15,10 @@ namespace BootCamp.Chapter1
         {
             return array != null && array.Length > 0;
         }
-
         internal static void Sort(int[] array)
         {
             if (IsNotNullAndEmpty(array)) Array.Sort(array);
         }
-        
         internal static void Reverse(int[] array)
         {
             if (IsNotNullAndEmpty(array)) Array.Reverse(array);
@@ -28,129 +26,90 @@ namespace BootCamp.Chapter1
 
         internal static int[] RemoveLast(int[] array)
         {
-            if (IsNotNullAndEmpty(array))
-            {
-                int[] modifiedArray = new int[array.Length - 1];
+            if (!IsNotNullAndEmpty(array)) return array;
 
-                Array.Copy(array, modifiedArray, array.Length - 1);
-
-                return modifiedArray;
-            }
-            else
-            {
-                return array;
-            }
+            int[] modifiedArray = new int[array.Length - 1];
+            Array.Copy(array, modifiedArray, array.Length - 1);
+            return modifiedArray;
         }
-        
-        internal static int[] InsertLast(int[] array, int number)
-        {
-            if (IsNotNullAndEmpty(array))
-            {
-
-                int[] modifiedArray = new int[array.Length + 1];
-                for (int i = 0; i < array.Length; i++)
-                {
-                    modifiedArray[i] = array[i];
-                }
-
-                modifiedArray.SetValue(number, modifiedArray.Length - 1);
-                return modifiedArray;
-            }
-            else
-            {
-                return new int[1] { number };
-            }
-        }
-
         internal static int[] RemoveFirst(int[] array)
         {
-            if (IsNotNullAndEmpty(array))
-            {
-                int[] modifiedArray = new int[array.Length - 1];
-
-                Array.Copy(array, 1, modifiedArray, 0, array.Length - 1);
-
-                return modifiedArray;
-            }
-            else
-            {
-                return array;
-            }
+            if (!IsNotNullAndEmpty(array)) return array;
+            
+            int[] modifiedArray = new int[array.Length - 1];
+            Array.Copy(array, 1, modifiedArray, 0, array.Length - 1);
+            return modifiedArray;
+            
         }
-        
-        internal static int[] InsertFirst(int[] array, int number)
-        {
-
-            if (IsNotNullAndEmpty(array))
-            {
-                int[] modifiedArray = new int[array.Length + 1];
-                modifiedArray.SetValue(number, 0);
-                for (int i = 0; i < array.Length; i++)
-                {
-                    modifiedArray[i + 1] = array[i];
-                }
-                return modifiedArray;
-            }
-            else
-            {
-                return new int[1] { number };
-            }
-        }
-
         internal static int[] RemoveAt(int[] array, int index)
         {
             bool HasInvalidArguments = array == null || index < 0 || index >= array.Length;
 
-            if (HasInvalidArguments)
+            if (HasInvalidArguments) return array;
+           
+            var modifiedArray = new int[array.Length - 1];
+            for (int i = 0; i < modifiedArray.Length; i++)
             {
-                return array;
+                modifiedArray[i] = i < index ? array[i] : array[i + 1];
             }
-            else
-            {
-                var modifiedArray = new int[array.Length - 1];
-                for (int i = 0; i < modifiedArray.Length; i++)
-                {
-                    modifiedArray[i] = i < index ? array[i] : array[i + 1];
-                }
-                return modifiedArray;
-            }
+            return modifiedArray;
         }
-        
+
+        internal static int[] InsertLast(int[] array, int number)
+        {
+            if (!IsNotNullAndEmpty(array)) return new int[1] { number };
+           
+            int[] modifiedArray = new int[array.Length + 1];
+            for (int i = 0; i < array.Length; i++)
+            {
+                modifiedArray[i] = array[i];
+            }
+
+            modifiedArray.SetValue(number, modifiedArray.Length - 1);
+            return modifiedArray;
+
+        }
+
+        internal static int[] InsertFirst(int[] array, int number)
+        {
+
+            if (!IsNotNullAndEmpty(array)) return new int[1] { number };
+                        
+            int[] modifiedArray = new int[array.Length + 1];
+            modifiedArray.SetValue(number, 0);
+            for (int i = 0; i < array.Length; i++)
+            {
+                modifiedArray[i + 1] = array[i];
+            }
+            return modifiedArray;
+            
+        }
+
         internal static int[] InsertAt(int[] array, int number, int index)
         {
             /// <returns>A new array with element inserted at a given index.
             /// If an array is empty or null, returns new array with number in it.</returns>
-
             if (index <= 0 || array.Length == 0)
             {
-                if (index == 0)
+                if (index == 0 ) return new int[1] { number };
+                return array;
+            }
+            
+            var modifiedArray = new int[array.Length + 1];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i == index)
                 {
-                    return new int[1] { number };
+                    modifiedArray[index] = number;
                 }
                 else
                 {
-                    return array;
+                    modifiedArray[i] = array[i];
                 }
 
             }
-            else
-            {
-                var modifiedArray = new int[array.Length + 1];
-                for (int i = 0; i < array.Length; i++)
-                {
-                    if (i == index)
-                    {
-                        modifiedArray[index] = number;
-                    }
-                    else
-                    {
-                        modifiedArray[i] = array[i];
-                    }
+            return modifiedArray;
 
-                }
-                return modifiedArray;
-
-            }
         }
     }
 }
